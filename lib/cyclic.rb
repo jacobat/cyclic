813051dc1a0288cb6634ed397fbf9d3c90958888
Jacob Atzen <jacob@jacobatzen.dk>
2009-05-18 00:51:07 +0200

    Documentation updated

--- lib/cyclic.rb
+++ lib/cyclic.rb
@@ -4,14 +4,18 @@ $:.unshift(File.dirname(__FILE__)) unless
 module Cyclic
   VERSION = '0.0.2'
   
+  # A cyclic buffer, that will discard elements as the buffer grows
+  # full.
   class Buffer
     include Enumerable
 
+    # Create a buffer holding the given _size_ number of elements
     def initialize(size)
       @size = size
       @buffer = []
     end
 
+    # Push any number of elements onto the buffer
     def push(*elements)
       @buffer.push(*elements)
       @buffer = @buffer[-@size, @size] if(@buffer.size > @size)
@@ -19,10 +23,12 @@ module Cyclic
 
     alias_method :<<, :push
 
+    # Return the contents of the buffer as an array
     def read
       @buffer
     end
     
+    # Loop over each element in the buffer
     def each(&blk)
       @buffer.each(&blk)
     end
